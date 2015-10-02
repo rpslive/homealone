f2316ad1d2055f24539212306f548358fab0bc64
rpslive <rps.live@gmail.com>
2015-10-02 05:54:40 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rpslive on 01/10/15.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    //console.log("request is " + req.user.username);
+    res.render('index', { title: "Chirp"});
+});
+
+module.exports = router;
